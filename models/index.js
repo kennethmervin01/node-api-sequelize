65e591700ab551eeac75f9fa2710542a93c1c1bd
kenneth mervin enriquez <kennethmervinenriquez@gmail.com>
2018-10-23 00:30:50 +0800

    dotenv and Babel Setup

---
+++ models/index.js
@@ -0,0 +1,37 @@
+"use strict";
+import fs from "fs";
+import path from "path";
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize("api_node", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+let db = {};
+
+fs.readdirSync(__dirname)
+  .filter(function(file) {
+    return file.indexOf(".") !== 0 && file !== "index.js";
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
---
+++ models/users.js
@@ -0,0 +1,64 @@
+// const bcrypt = require("bcrypt-nodejs");
+import bcrypt from "bcrypt-nodejs";
+export default (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    username: {
+      type: Sequelize.STRING(16),
+      allowNull: false,
+      unique: true
+    },
+
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      },
+      unique: true
+    }
+  });
+
+  // User.associate = models => {
+  //   User.hasMany(models.password, {
+  //     foreignKey: "user_id",
+  //     sourceKey: "id"
+  //   });
+  // };
+
+  User.beforeSave = async (user, options) => {
+    let err;
+    if (user.changed("password")) {
+      let salt, err;
+      [err, salt] = await bcrypt.genSaltSync(10);
+      if (err) console.log("Cant Save User Error 1");
+      [err, hash] = await bcrypt.hashSync(user.password, salt);
+      if (err) console.log("Cant Save User Error 2");
+      user.password = hash;
+    }
+  };
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.hook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+
+  return User;
+};
